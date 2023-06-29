695962a834547ad4ea5a4c731ee7cc22163fa503
arnold-aswan <arnold.aswani@student.moringaschool.com>
2023-06-29 11:18:23 +0300

    completed algorithms palindrome solutions 2

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  for(let i = 0; i<word.length/2; i++) {
+
+    const j = word.length - 1 - i
+    
+    if(word[i] !== word[j]) { return false }
+  }
+  return true
 }
 
 /* 
@@ -15,6 +22,26 @@ if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
 
   console.log("");
 
